c7e5ab20b06d309d0d8e6bec7d7f156664776a5e
yeetnbeet <segalcsam@gmail.com>
2023-05-05 15:31:12 -0600

    init

---
+++ app.py
@@ -0,0 +1,50 @@
+import json
+import os
+from flask import Flask, request
+import smtplib
+from email.mime.text import MIMEText
+
+app = Flask(__name__)
+
+# Replace the following with your own email credentials and settings
+EMAIL_SERVER = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USER = 'sam@contenderbicycles.com'
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
+FROM_EMAIL = 'sam@contenderbicycles.com'
+TO_EMAIL = 'service@contenderbicycles.com'
+
+# Replace this with the product ID(s) you want to monitor
+MONITORED_PRODUCT_IDS = [1234567890]
+
+
+@app.route('/webhook', methods=['POST'])
+def handle_webhook():
+    data = json.loads(request.data)
+    line_items = data['line_items']
+    
+    for item in line_items:
+        if item['product_id'] in MONITORED_PRODUCT_IDS:
+            send_email_notification(item, data['order_number'])
+            break
+
+    return '', 200
+
+
+def send_email_notification(item, order_number):
+    subject = f"Product Sold: {item['title']}"
+    body = f"Order Number: {order_number} - {item['title']} has been sold. Quantity: {item['quantity']}"
+
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = FROM_EMAIL
+    msg['To'] = TO_EMAIL
+
+    with smtplib.SMTP(EMAIL_SERVER, EMAIL_PORT) as server:
+        server.starttls()
+        server.login(EMAIL_USER, EMAIL_PASSWORD)
+        server.send_message(msg)
+
+
+if __name__ == '__main__':
+    app.run()
